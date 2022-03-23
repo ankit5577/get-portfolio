ede27bd266c40102626d4ba9267b45cd9146e2b8
Ankit Kaushal <kaushalankit101010@gmail.com>
2022-03-23 21:14:27 +0530

    final build

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -13,54 +13,54 @@ function Contact() {
         </div>
         <div className="flex flex-row gap-4 flex-wrap">
           <div className="flex-1 p-4">
-            <div className="relative flex flex-col md:flex-row flex-wrap my-2 md:my-4">
-              <div className="flex-3 md:self-center align-middle w-[140px] lg:text-2xl lg:w-[160px] md:p-4 text-xl font-bold text-slate-500">
+            <div className="relative flex flex-col md:flex-row flex-wrap my-2 lg:my-4">
+              <div className="flex-3 md:self-center align-middle w-[140px] lg:text-2xl lg:w-[160px] xl:p-4 text-xl font-bold text-slate-500">
                 <span className="text-slate-400">GitHub</span>
               </div>
               <a
-                href="https://www.aiboost.in/"
+                href="https://github.com/ankit5577"
                 target="_blank"
                 rel="noreferrer"
                 className="hover:cursor-pointer rounded-lg flex flex-row gap-1"
               >
-                <div className="flex-1 md:ml-4 p-4 bg-slate-800/3 rounded-lg shadow-md backdrop-blur-md">
-                  <h4 className="antialiased tracking-wide md:tracking-wider font-medium text-sm sm:text-md md:text-lg">
+                <div className="flex-1 lg:ml-4 p-4 bg-slate-800/3 rounded-lg shadow-md backdrop-blur-md">
+                  <h4 className="antialiased tracking-wide md:tracking-wider font-medium text-sm sm:text-md xl:text-lg">
                     ankit5577
                   </h4>
                 </div>
               </a>
             </div>
 
-            <div className="relative flex flex-col md:flex-row flex-wrap my-2 md:my-4">
-              <div className="flex-3 md:self-center align-middle w-[140px] lg:text-2xl lg:w-[160px] md:p-4 text-xl font-bold text-slate-500">
+            <div className="relative flex flex-col md:flex-row flex-wrap my-2 lg:my-4">
+              <div className="flex-3 md:self-center align-middle w-[140px] lg:text-2xl lg:w-[160px] xl:p-4 text-xl font-bold text-slate-500">
                 <span className="text-slate-400">CodeChef</span>
               </div>
               <a
-                href="https://www.aiboost.in/"
+                href="https://www.codechef.com/users/ankit5577"
                 target="_blank"
                 rel="noreferrer"
                 className="hover:cursor-pointer rounded-lg flex flex-row gap-1"
               >
-                <div className="flex-1 md:ml-4 p-4 bg-slate-800/3 rounded-lg shadow-md backdrop-blur-md">
-                  <h4 className="antialiased tracking-wide md:tracking-wider font-medium text-sm sm:text-md md:text-lg">
+                <div className="flex-1 lg:ml-4 p-4 bg-slate-800/3 rounded-lg shadow-md backdrop-blur-md">
+                  <h4 className="antialiased tracking-wide md:tracking-wider font-medium text-sm sm:text-md xl:text-lg">
                     ankit5577
                   </h4>
                 </div>
               </a>
             </div>
 
-            <div className="relative flex flex-col md:flex-row flex-wrap my-2 md:my-4">
-              <div className="flex-3 md:self-center align-middle w-[140px] lg:text-2xl lg:w-[160px] md:p-4 text-xl font-bold text-slate-500">
+            <div className="relative flex flex-col md:flex-row flex-wrap my-2 lg:my-4">
+              <div className="flex-3 md:self-center align-middle w-[140px] lg:text-2xl lg:w-[160px] xl:p-4 text-xl font-bold text-slate-500">
                 <span className="text-slate-400">LinkedIn</span>
               </div>
               <a
-                href="https://www.aiboost.in/"
+                href="https://www.linkedin.com/in/ankit5577/"
                 target="_blank"
                 rel="noreferrer"
                 className="hover:cursor-pointer rounded-lg flex flex-row gap-1"
               >
-                <div className="flex-1 md:ml-4 p-4 bg-slate-800/3 rounded-lg shadow-md backdrop-blur-md">
-                  <h4 className="antialiased tracking-wide md:tracking-wider font-medium text-sm sm:text-md md:text-lg">
+                <div className="flex-1 lg:ml-4 p-4 bg-slate-800/3 rounded-lg shadow-md backdrop-blur-md">
+                  <h4 className="antialiased tracking-wide md:tracking-wider font-medium text-sm sm:text-md xl:text-lg">
                     ankit5577
                   </h4>
                 </div>
@@ -68,18 +68,18 @@ function Contact() {
             </div>
           </div>
           <div className="flex-1 border-l border-green-500 p-4">
-            <div className="relative flex flex-col md:flex-row flex-wrap my-2 md:my-4">
-              <div className="flex-3 md:self-center align-middle w-[140px] lg:text-2xl lg:w-[160px] md:p-4 text-xl font-bold text-slate-500">
+            <div className="relative flex flex-col md:flex-row flex-wrap my-2 lg:my-4">
+              <div className="flex-3 md:self-center align-middle w-[140px] lg:text-2xl lg:w-[160px] xl:p-4 text-xl font-bold text-slate-500">
                 <span className="text-slate-400">Email</span>
               </div>
               <a
-                href="https://www.aiboost.in/"
+                href="mailto:ankit5577.dev@gmail.com"
                 target="_blank"
                 rel="noreferrer"
                 className="hover:cursor-pointer rounded-lg flex flex-row gap-1"
               >
-                <div className="flex-1 md:ml-4 p-4 bg-slate-800/3 rounded-lg shadow-md backdrop-blur-md">
-                  <h4 className="antialiased tracking-wide md:tracking-wider font-medium text-sm sm:text-md md:text-lg">
+                <div className="flex-1 lg:ml-4 p-4 bg-slate-800/3 rounded-lg shadow-md backdrop-blur-md">
+                  <h4 className="antialiased tracking-wide md:tracking-wider font-medium text-sm sm:text-md xl:text-lg">
                     ankit5577.dev@gmail.com
                   </h4>
                 </div>
@@ -87,39 +87,32 @@ function Contact() {
             </div>
 
             <div className="relative flex flex-col md:flex-row flex-wrap  my-2 md:my-4">
-              <div className="flex-3 md:self-center align-middle w-[140px] lg:text-2xl lg:w-[160px] md:p-4 text-xl font-bold text-slate-500">
+              <div className="flex-3 md:self-center align-middle w-[140px] lg:text-2xl lg:w-[160px] xl:p-4 text-xl font-bold text-slate-500">
                 <span className="text-slate-400">Phone</span>
               </div>
               <a
-                href="https://www.aiboost.in/"
+                href="tel:+917307844227"
                 target="_blank"
                 rel="noreferrer"
                 className="hover:cursor-pointer rounded-lg flex flex-row gap-1"
               >
-                <div className="flex-1 md:ml-4 p-4 bg-slate-800/3 rounded-lg shadow-md backdrop-blur-md">
-                  <h4 className="antialiased tracking-wide md:tracking-wider font-medium text-sm sm:text-md md:text-lg">
+                <div className="flex-1 lg:ml-4 p-4 bg-slate-800/3 rounded-lg shadow-md backdrop-blur-md">
+                  <h4 className="antialiased tracking-wide md:tracking-wider font-medium text-sm sm:text-md xl:text-lg">
                     +91 730 7844227
                   </h4>
                 </div>
               </a>
             </div>
 
-            <div className="relative flex flex-col md:flex-row flex-wrap my-2 md:my-4">
-              <div className="flex-3 md:self-center align-middle w-[140px] lg:text-2xl lg:w-[160px] md:p-4 text-xl font-bold text-slate-500">
+            <div className="relative flex flex-col md:flex-row flex-wrap my-2 lg:my-4">
+              <div className="flex-3 md:self-center align-middle w-[140px] lg:text-2xl lg:w-[160px] xl:p-4 text-xl font-bold text-slate-500">
                 <span className="text-slate-400">Skype</span>
               </div>
-              <a
-                href="https://www.aiboost.in/"
-                target="_blank"
-                rel="noreferrer"
-                className="hover:cursor-pointer rounded-lg flex flex-row gap-1"
-              >
-                <div className="flex-1 md:ml-4 p-4 bg-slate-800/3 rounded-lg shadow-md backdrop-blur-md">
-                  <h4 className="antialiased tracking-wide md:tracking-wider font-medium text-sm sm:text-md md:text-lg">
-                    kaushalankit101010
-                  </h4>
-                </div>
-              </a>
+              <div className="flex-1 lg:ml-4 p-4 bg-slate-800/3 rounded-lg shadow-md backdrop-blur-md">
+                <h4 className="antialiased tracking-wide md:tracking-wider font-medium text-sm sm:text-md xl:text-lg">
+                  kaushalankit101010
+                </h4>
+              </div>
             </div>
           </div>
         </div>
